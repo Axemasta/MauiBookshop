e0d35b923aeebbd7ab02005670961cf302c70e17
Axemasta <33064621+Axemasta@users.noreply.github.com>
2023-07-13 10:40:18 +0100

    Book Detail UI
    The book details now get shown, unfortunately the iOS appearance doesnt render correctly so I've had to do a blurry background

---
+++ src/Bookshop/Behaviors/BlurBehavior.cs
@@ -0,0 +1,25 @@
+using System;
+namespace Bookshop.Behaviors;
+
+// https://github.com/VladislavAntonyuk/MauiSamples/blob/main/MauiImageEffects/BlurBehavior/BlurBehavior.cs
+public partial class BlurBehavior
+{
+	public static readonly BindableProperty RadiusProperty = BindableProperty.Create(nameof(Radius), typeof(float), typeof(BlurBehavior), 10f, propertyChanged: OnRadiusChanged);
+
+	public float Radius
+	{
+		get => (float)GetValue(RadiusProperty);
+		set => SetValue(RadiusProperty, value);
+	}
+
+	static void OnRadiusChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		var behavior = (BlurBehavior)bindable;
+		if (behavior.imageView is null)
+		{
+			return;
+		}
+
+		behavior.SetRendererEffect(behavior.imageView, Convert.ToSingle(newValue));
+	}
+}
---
+++ src/Bookshop/Platforms/Android/Behaviors/BlurBehavior.cs
@@ -0,0 +1,40 @@
+using System;
+using Android.Graphics;
+using Android.Widget;
+
+namespace Bookshop.Behaviors;
+
+public partial class BlurBehavior : PlatformBehavior<Image, ImageView>
+{
+	ImageView? imageView;
+	protected override void OnAttachedTo(Image bindable, ImageView platformView)
+	{
+		imageView = platformView;
+		SetRendererEffect(platformView, Radius);
+	}
+
+	protected override void OnDetachedFrom(Image bindable, ImageView platformView)
+	{
+		SetRendererEffect(platformView, 0);
+	}
+
+	void SetRendererEffect(ImageView imageView, float radius)
+	{
+		if (OperatingSystem.IsAndroidVersionAtLeast(31))
+		{
+			var renderEffect = radius > 0 ? GetEffect(radius) : null;
+			imageView.SetRenderEffect(renderEffect);
+		}
+		else
+		{
+
+		}
+	}
+
+	static RenderEffect? GetEffect(float radius)
+	{
+		return OperatingSystem.IsAndroidVersionAtLeast(31) ?
+			RenderEffect.CreateBlurEffect(radius, radius, Shader.TileMode.Decal!) :
+			null;
+	}
+}
